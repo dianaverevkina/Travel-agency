1aac41ffd469ccab7481e72b4b42011b6a200584
dianaverevkina <108188896+dianaverevkina@users.noreply.github.com>
2023-01-28 02:14:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 let icon = document.querySelectorAll('.icon');
 let menu__items = document.querySelector('.menu__items');
 let back = document.querySelector('body');
+let html = document.querySelector('html');
 
 icon.forEach(function (item) {
    item.onclick = function () {
       item.classList.toggle('active');
       menu__items.classList.toggle('active');
       back.classList.toggle('lock');
+      html.classList.toggle('lock');
    }
 });
