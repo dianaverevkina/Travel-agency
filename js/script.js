ceea701cd74bf75e6c5d4ecd24ed8ef7a78a1b49
dianaverevkina <108188896+dianaverevkina@users.noreply.github.com>
2023-01-31 23:24:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
 let icon = document.querySelectorAll('.icon');
 let menu__items = document.querySelector('.menu__items');
 let back = document.querySelector('body');
-let html = document.querySelector('html');
 
 icon.forEach(function (item) {
    item.onclick = function () {
       item.classList.toggle('active');
       menu__items.classList.toggle('active');
       back.classList.toggle('lock');
-      html.classList.toggle('lock');
    }
 });
+
+//Подключение слайдера
+
+let swiper = new Swiper('.slider', {
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   keybord: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+   },
+
+   watchOverflow: true,
+
+   loop: true,
+
+   loopedSlides: 4,
+
+   speed: 700,
+
+   breakpoints: {
+      200: {
+         slidesPerView: 1,
+         spaceBetween: 200,
+         centeredSlides: true,
+      },
+      768: {
+         slidesPerView: 2,
+           spaceBetween: 20,
+      },
+   },
+})
